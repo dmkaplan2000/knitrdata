d9beeec002d196441cbf2acee46ef725ccaaaf07
dmkaplan2000 <David M. Kaplan>
2020-10-25 00:46:05 +0200

    Improving tests and documentation of text chunks with md5sum checks.

--- tests/test.md5sum_chunks.R
+++ tests/test.md5sum_chunks.R
@@ -6,11 +6,21 @@ owd = getwd()
 td = tempdir()
 setwd(td)
 
-# Create a simple text file ------------------
-txt = paste0("Hello",platform.newline(),"Goodbye",platform.newline())
+# Create simple text files ------------------
+
+# Text file with platform line separator
+pn = platform.newline()
+txt = paste0("Hello",pn,"Goodbye",pn)
 txt.fn = "test.md5sum_chunks.txt"
 writeChar(txt,txt.fn,useBytes=TRUE,eos=NULL)
 txt.md5 = tools::md5sum(txt.fn)
+
+# Text file with opposite line separator
+opn = ifelse(pn=="\n","\r\n","\n")
+txt2 = paste0("Goodbye",opn,"Hello",opn)
+txt2.fn = "test2.md5sum_chunks.txt"
+writeChar(txt2,txt2.fn,useBytes=TRUE,eos=NULL)
+txt2.md5 = tools::md5sum(txt2.fn)
 
 # Create a simple binary file ----------------
 d = data.frame(x=1:3,y=letters[1:3])
@@ -25,10 +35,10 @@ if (file.exists(rmd.fn))
 rmarkdown::draft(rmd.fn,"github_document","rmarkdown",edit=FALSE)
 
 # Create chunks ------------------
-
 loadlib = create_chunk("library(knitrdata)",chunk_type="r",chunk_label="loadlib")
 
 lines = readLines(txt.fn)
+lines2 = readLines(txt2.fn)
 
 co = paste0('md5sum="',txt.md5,'"')
 loadtxt =
@@ -37,10 +47,26 @@ loadtxt =
                chunk_label="loadtxt",chunk_options_string = co
   )
 
+# Text chunk specifying explicitly newline character to use
+oopn = sub("\n","\\\\n",sub("\r","\\\\r",opn))
+co2 = paste0('md5sum="',txt2.md5,'",line.sep="',oopn,'"')
+loadtxt2 =
+  create_chunk(lines2,output.var="txt2",output.file="output2.md5sum_chunks.txt",
+               format="text",echo=TRUE,
+               chunk_label="loadtxt2",chunk_options_string = co2
+  )
+
 loadtxt64 = data_encode(txt.fn,"base64") %>%
   create_chunk(output.var="txt64",output.file="output.md5sum_chunks.64.txt",
                format="text",echo=TRUE,encoding="base64",
                chunk_label="loadtxt64",chunk_options_string = co
+  )
+
+co2 = paste0('md5sum="',txt2.md5,'"')
+loadtxt264 = data_encode(txt2.fn,"base64") %>%
+  create_chunk(output.var="txt264",output.file="output2.md5sum_chunks.64.txt",
+               format="text",echo=TRUE,encoding="base64",
+               chunk_label="loadtxt264",chunk_options_string = co2
   )
 
 co = paste0('md5sum="',rds.md5,'"')
@@ -50,7 +76,8 @@ loaddata = data_encode(rds.fn,"base64") %>%
                chunk_label="loadrds",chunk_options_string = co
   )
 
-showdata = create_chunk("cat(txt)\ncat(txt64)\nd",chunk_type="r",
+showdata = create_chunk("cat(txt)\ncat(txt2)\ncat(txt64)\ncat(txt264)\nd",
+                        chunk_type="r",
                         chunk_label="showdata")
 
 # Push chunks into document ------------------
@@ -60,7 +87,9 @@ rmd.text = readLines(rmd.fn)
 
 rmd.text = insert_chunk(showdata,11,rmd.text=rmd.text)
 rmd.text = insert_chunk(loaddata,11,rmd.text=rmd.text)
+rmd.text = insert_chunk(loadtxt264,11,rmd.text=rmd.text)
 rmd.text = insert_chunk(loadtxt64,11,rmd.text=rmd.text)
+rmd.text = insert_chunk(loadtxt2,11,rmd.text=rmd.text)
 rmd.text = insert_chunk(loadtxt,11,rmd.text=rmd.text)
 rmd.text = insert_chunk(loadlib,11,rmd.text=rmd.text)
 
